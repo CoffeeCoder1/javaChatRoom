ec8de4a2ca9c80420385fec3b8f3219021a712e2
CoffeeCoder1 <76572535+CoffeeCoder1@users.noreply.github.com>
2021-03-29 13:47:22 -0400

    Revert "Revert "Create working JAR files for server and client""
    This reverts commit 1ce1991377310d01f0134b2638d853aeed760b94.

--- SRC/javaChatRoomClient/src/main/java/io/github/CoffeeCoder1/javaChatRoomClient/Client.java
+++ SRC/javaChatRoomClient/src/main/java/io/github/CoffeeCoder1/javaChatRoomClient/Client.java
@@ -23,7 +23,7 @@
  * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
  * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
  * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
- * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
  * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
  * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
@@ -38,8 +38,7 @@ public class Client {
         
         // Print error if not all arguments are supplied [default hostname and port coming soon]
         if (args.length != 2) {
-            System.err.println(
-                "Usage: java Client <host name> <port number>");
+            System.err.println("Usage: java Client <host name> <port number>");
             System.exit(1);
         }
         
